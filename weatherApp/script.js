1379fc4c49be51df239b1df17b8a3ee4efe5bec6
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-01-28 16:37:59 +0530

    completed basic weatherApi

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -10,11 +10,17 @@ async function getData(cityName) {
         `http://api.weatherapi.com/v1/current.json?key=4224cec53c9c4aa48e680405242701&q=${cityName}&aqi=yes`
         )
         return await promise.json();
+        //use `` to enter a variable in the url of the api 
 }
 
 button.addEventListener('click', async () => {
     // console.log(city.value);// to ge the value entered in the form
-    const result = await city.value;
-    
+    // const value = city.value;
+    const result = await getData(city.value);
+    console.log(result);
 
+    // have to use `` for writing part of E6
+    cityName.innerText = `Location: ${result.location.name}, ${result.location.region} - ${result.location.country}`;
+    cityTime.innerText = `Local Time: ${result.location.localtime}`; 
+    cityTemp.innerText = `Temperature: ${result.current.temp_c}`;
 });
